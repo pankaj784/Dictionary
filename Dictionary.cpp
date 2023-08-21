e75f3686ea9da70696488936764bb9b031fabd2b
pankaj784 <bhadauriyapankaj11@gmail.com>
2023-08-21 22:46:06 +0530

    final commit

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define ALPHABET_SIZE 26
 #define false 0
 #define true 1
-#define MAX_WORD_SIZE   50
+#define MAX_WORD_SIZE 50
 #define MAX_MEANING_SIZE 1000
 
 int letterToInt(char letter)
@@ -25,14 +25,14 @@ int letterToInt(char letter)
 
 struct trieNode
 {
-    struct trieNode* children[ALPHABET_SIZE];
+    struct trieNode *children[ALPHABET_SIZE];
     bool isEndOfWord;
     string value;
 };
 
-struct trieNode* getNode(void)
+struct trieNode *getNode(void)
 {
-    struct trieNode *pNode =  new trieNode;
+    struct trieNode *pNode = new trieNode;
 
     pNode->isEndOfWord = false;
 
@@ -62,7 +62,7 @@ int insert(struct trieNode *root, string word, string meaning)
     return true;
 }
 
-int readWordsFromFile(struct trieNode *root, const char* filename)
+int readWordsFromFile(struct trieNode *root, const char *filename)
 {
     FILE *file = fopen(filename, "r");
 
@@ -111,7 +111,8 @@ bool search(struct trieNode *root, string word)
         pCrawl = pCrawl->children[index];
     }
 
-    if (pCrawl != NULL && pCrawl->isEndOfWord)  {
+    if (pCrawl != NULL && pCrawl->isEndOfWord)
+    {
         cout << "The meaning of the word is: \n";
         cout << pCrawl->value;
     }
@@ -133,61 +134,71 @@ bool deleteWord(struct trieNode *root, string word)
         pCrawl = pCrawl->children[index];
     }
 
-    if (pCrawl != NULL && pCrawl->isEndOfWord)  {
+    if (pCrawl != NULL && pCrawl->isEndOfWord)
+    {
         pCrawl->isEndOfWord = false;
         return true;
     }
 }
 
-void alphabeticalOrder(struct trieNode* root, char allWords[], int index)
+void alphabeticalOrder(struct trieNode *root, char allWords[], int index)
 {
-    struct trieNode* pCrawl = root;
+    struct trieNode *pCrawl = root;
 
-    if(pCrawl != NULL && pCrawl->isEndOfWord == true) {
-        for(int j = 0; j < index; j++)  {
+    if (pCrawl != NULL && pCrawl->isEndOfWord == true)
+    {
+        for (int j = 0; j < index; j++)
+        {
             cout << allWords[j];
-            }
+        }
 
         cout << ": ";
         cout << pCrawl->value << "\n";
     }
 
-    for(int i = 0; i < ALPHABET_SIZE; i++)  {
-        if(pCrawl->children[i] != NULL) {
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (pCrawl->children[i] != NULL)
+        {
             allWords[index] = 'a' + i;
             alphabeticalOrder(pCrawl->children[i], allWords, index + 1);
         }
     }
 }
 
-void print_prefix_search(struct trieNode* root, char allWords[], int index, string prefix)
+void print_prefix_search(struct trieNode *root, char allWords[], int index, string prefix)
 {
-    struct trieNode* pCrawl = root;
+    struct trieNode *pCrawl = root;
 
-    if(pCrawl != NULL && pCrawl->isEndOfWord == true) {
+    if (pCrawl != NULL && pCrawl->isEndOfWord == true)
+    {
         cout << prefix;
 
-        for(int j = 0; j < index; j++)  {
+        for (int j = 0; j < index; j++)
+        {
             cout << allWords[j];
-            }
+        }
 
         cout << ": ";
         cout << pCrawl->value << "\n";
     }
 
-    for(int i = 0; i < ALPHABET_SIZE; i++)  {
-        if(pCrawl->children[i] != NULL) {
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (pCrawl->children[i] != NULL)
+        {
             allWords[index] = 'a' + i;
             print_prefix_search(pCrawl->children[i], allWords, index + 1, prefix);
         }
     }
 }
 
-void prefix_search(struct trieNode* root, string prefix)
+void prefix_search(struct trieNode *root, string prefix)
 {
-    struct trieNode* pCrawl = root;
+    struct trieNode *pCrawl = root;
 
-    for(int i = 0; i < prefix.length(); i++)    {
+    for (int i = 0; i < prefix.length(); i++)
+    {
         int index = letterToInt(prefix[i]);
         pCrawl = pCrawl->children[index];
     }
@@ -196,9 +207,9 @@ void prefix_search(struct trieNode* root, string prefix)
     print_prefix_search(pCrawl, allWords, 0, prefix);
 }
 
-bool searchSuggestedWord(struct trieNode* root, string word, int count)
+bool searchSuggestedWord(struct trieNode *root, string word, int count)
 {
-     struct trieNode *pCrawl = root;
+    struct trieNode *pCrawl = root;
 
     for (int i = 0; i < word.length(); i++)
     {
@@ -210,8 +221,10 @@ bool searchSuggestedWord(struct trieNode* root, string word, int count)
         pCrawl = pCrawl->children[index];
     }
 
-    if (pCrawl != NULL && pCrawl->isEndOfWord)  {
-        if(count == 0)  {
+    if (pCrawl != NULL && pCrawl->isEndOfWord)
+    {
+        if (count == 0)
+        {
             cout << "Suggested words are: ";
         }
         cout << word << "  ";
@@ -219,52 +232,21 @@ bool searchSuggestedWord(struct trieNode* root, string word, int count)
 
     return true;
 }
-
-bool suggestedWords(struct trieNode* root, string word, int number)
+int main()
 {
-    int count = 0;
-
-    for(int i = 0; i < word.length(); i++)  {
-        string temp = word;
-
-        for(int j = 0; j < ALPHABET_SIZE; j++)  {
-            if(count < number && j != letterToInt(word[word.length() - i - 1]))  {
-                temp[word.length() - i - 1] = j + 97;
-
-                if(searchSuggestedWord(root, temp, count)) {
-                    count++;
-                }
-            }
-        }
-    }
-
-    if(count == 0)  {
-            cout << "\nSorry we couldn't find any words related to " << word << " in our Dictionary.\n";
-        return false;
-    }
-    else if (count < number)    {
-        cout << "\nWe could only find " << count << " words related to " << word << " in our Dictionary.\n";
-        return true;
-    }
-    else    {
-        return true;
-    }
-}
-
-int main()  {
     struct trieNode *root = getNode();
 
     readWordsFromFile(root, "words.txt");
 
     string command;
-    do  {
+    do
+    {
         cout << "\nWhat would you like to do?\n";
         cout << "1. Insert a word.\n";
         cout << "2. Search a word.\n";
         cout << "3. Delete a word.\n";
         cout << "4. Print dictionary in alphabetical order.\n";
         cout << "5. Prefix search.\n";
-        cout << "6. Show suggested words.\n";
         cout << "\n";
         fflush(stdin);
 
@@ -275,84 +257,86 @@ int main()  {
         string word;
         string meaning;
 
-        switch(call)    {
+        switch (call)
+        {
 
-            case 1: cout << "Enter the word you would like to insert: ";
-                    cin >> word;
+        case 1:
+            cout << "Enter the word you would like to insert: ";
+            cin >> word;
+            cout << "Enter its meaning: ";
+            cin >> meaning;
+
+            if (insert(root, word, meaning))
+            {
+                cout << word << " has been added to the Dictionary.\n";
+            }
+
+            break;
+
+        case 2:
+            cout << "Enter the word you would like to search: ";
+            cin >> word;
+
+            if (!search(root, word))
+            {
+                cout << "Sorry, the word you searched for doesn't exist. Would you like to add it to the Dictionary.(Yes/No) ";
+
+                string add;
+                cin >> add;
+
+                if (add == "yes" || add == "Yes" || command == "YES")
+                {
                     cout << "Enter its meaning: ";
                     cin >> meaning;
 
-                    if(insert(root, word, meaning)) {
+                    if (insert(root, word, meaning))
+                    {
                         cout << word << " has been added to the Dictionary.\n";
                     }
+                }
+            }
 
-                    break;
+            break;
 
-            case 2: cout << "Enter the word you would like to search: ";
-                    cin >> word;
+        case 3:
+            cout << "Which word would you like to delete? ";
+            cin >> word;
 
-                    if(!search(root, word))  {
-                        cout << "Sorry, the word you searched for doesn't exist. Would you like to add it to the Dictionary.(Yes/No) ";
+            if (deleteWord(root, word))
+            {
+                cout << word << " has been successfully deleted from the Dictionary.\n";
+            }
 
-                        string add;
-                        cin >> add;
+            else
+            {
+                cout << "No such word exists in the Dictionary.\n";
+            }
 
-                        if(add == "yes" || add == "Yes" || command == "YES")  {
-                            cout << "Enter its meaning: ";
-                            cin >> meaning;
+            break;
 
-                            if(insert(root, word, meaning)) {
-                            cout << word << " has been added to the Dictionary.\n";
-                            }
-                        }
-                    }
+        case 4:
+            char allWords[50];
+            alphabeticalOrder(root, allWords, 0);
+            cout << "\n";
 
-                    break;
+            break;
 
-            case 3: cout << "Which word would you like to delete? ";
-                    cin >> word;
+        case 5:
+            cout << "Enter the word you would like to use as a prefix: ";
+            cin >> word;
+            cout << "\n";
 
-                    if(deleteWord(root, word))  {
-                        cout << word << " has been successfully deleted from the Dictionary.\n";
-                    }
+            prefix_search(root, word);
 
-                    else    {
-                        cout << "No such word exists in the Dictionary.\n";
-                    }
+            break;
 
-                    break;
-
-            case 4: char allWords[50];
-                    alphabeticalOrder(root, allWords, 0);
-                    cout << "\n";
-
-                    break;
-
-            case 5: cout << "Enter the word you would like to use as a prefix: ";
-                    cin >> word;
-                    cout << "\n";
-
-                    prefix_search(root, word);
-
-                    break;
-
-            case 6: cout << "Enter the word for whose suggested words you want to see: ";
-                    cin >> word;
-                    int number;
-                    cout << "How many suggested words do you want to see: ";
-                    cin >> number;
-
-                    suggestedWords(root, word, number);
-
-                    break;
-
-            default: cout << "Enter a valid entry.";
+        default:
+            cout << "Enter a valid entry.";
         }
-    fflush(stdin);
+        fflush(stdin);
         cout << "\n\nWould you like to continue or exit?(Yes/N0) ";
         cin >> command;
-    }
-    while(command == "yes" || command == "Yes" || command == "YES");
+    } while (command == "yes" || command == "Yes" || command == "YES");
 
     cout << "Thanks for using our Dictionary.\n";
     return 0;
